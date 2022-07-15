2a80d0aa09b758e710b594a148bdb8c00ef4cfb0
Edwin Boon <edwin.boon@eo.nl>
2022-07-15 09:05:33 +0200

    Add basic setup for hangman. Ask user for input and compare it with the chosen word. Fill out any blanks if the letter is in the chosen word

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-#Step 1 
+from random import choice
 
-word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = choice(word_list)
+word_length = len(chosen_word)
+display = []
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+for _ in range(word_length):
+  display.append("_")
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+guess = input("Guess a letter: ").lower()
+
+for position in range(word_length):
+  letter = chosen_word[position]
+  if (guess == letter):
+    display[position] = letter
+
+print(display)
